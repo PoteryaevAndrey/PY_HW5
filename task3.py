61d5102947c2e9ab04f55f938b9decb5224fd28f
andrey <ai.poteryaev@gmail.com>
2023-01-09 15:10:52 +0500

    task3

---
+++ task3.py
@@ -0,0 +1,25 @@
+# with open('rle.txt', 'r') as data:
+with open('HW5/rle.txt', 'r') as data:
+    my_text = data.read()
+
+def encode_rle(ss):
+    str_code = ''
+    prev_char = ''
+    count = 1
+    for char in ss:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    return str_code
+
+            
+str_code = encode_rle(my_text)
+print(str_code)
+dev_12 = str_code
+
+with open('rle_encod.txt', 'w') as data:
+    data.write(dev_12)
